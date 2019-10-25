dc75c07cecffcfcbeee977b6cb8ad920cb1ee017
mgxian <will835559313@163.com>
2019-10-25 08:58:06 +0800

    add more test for WSClientStore

--- gateway/wsclientstore_test.go
+++ gateway/wsclientstore_test.go
@@ -2,6 +2,22 @@ package gateway
 
 import "testing"
 
-func TestWSClientStores(t *testing.T) {
+func TestWSClientStore(t *testing.T) {
+	imApp := "im"
+	chatApp := "chat"
+	matchApp := "match"
+	imMemberID := 123456
+	notConnectedImMemberID := 123
+	store := NewWSClientStore()
+	ws1 := newStubWSConn("1")
+	ws2 := newStubWSConn("2")
+	store.save(matchApp, -1, ws1)
+	store.save(matchApp, -1, ws2)
+	store.save(imApp, imMemberID, ws2)
 
+	assertWSclientCount(t, len(store.publicWSClientsForApp(chatApp)), 0)
+	assertWSclientCount(t, len(store.publicWSClientsForApp(matchApp)), 2)
+
+	assertWSclientCount(t, len(store.privateWSClientsForMember(notConnectedImMemberID)), 0)
+	assertWSclientCount(t, len(store.privateWSClientsForMember(imMemberID)), 1)
 }
